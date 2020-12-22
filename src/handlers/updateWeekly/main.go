6583c0d4957c15e083ac55e0e8b08168ce1bd304
kou-pg-0131 <kou.pg.0131@gmail.com>
2020-12-22 22:22:48 +0900

    Controller.UpdateWeekly を実行できるようにした

--- src/handlers/updateWeekly/main.go
+++ src/handlers/updateWeekly/main.go
@@ -1,12 +1,22 @@
 package main
 
 import (
+	"time"
+
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/kou-pg-0131/qiita-lgtm-ranking/src/interfaces/controllers"
 )
 
+// Event .
+type Event struct {
+	ItemID string `json:"item_id"`
+	Tag    string `json:"tag"`
+}
+
 // Handler .
-func Handler() error {
-	return nil
+func Handler(ev *Event) error {
+	c := controllers.NewItemsControllerFactory().Create()
+	return c.UpdateWeekly(time.Now(), ev.ItemID, ev.Tag)
 }
 
 func main() {
