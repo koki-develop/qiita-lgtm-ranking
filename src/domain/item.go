93015fd533de7191219b11d9fd7d6367cb08b7ca
kou-pg-0131 <kou.pg.0131@gmail.com>
2020-12-23 09:28:53 +0900

    Item のフィールドを追加、 User, Tag を定義

--- src/domain/item.go
+++ src/domain/item.go
@@ -1,9 +1,30 @@
 package domain
 
+import "time"
+
 // Item .
 type Item struct {
-	Title string `json:"title"`
+	Title      string    `json:"title"`
+	LikesCount int       `json:"likes_count"`
+	URL        string    `json:"url"`
+	User       User      `json:"user"`
+	Tags       Tags      `json:"tags"`
+	CreatedAt  time.Time `json:"created_at"`
 }
 
 // Items .
 type Items []*Item
+
+// Tag .
+type Tag struct {
+	Name string `json:"name"`
+}
+
+// Tags .
+type Tags []*Tag
+
+// User .
+type User struct {
+	ID              string `json:"id"`
+	ProfileImageURL string `json:"profile_image_url"`
+}
