e001c0dc5ab521674524b0b5815396a6c8de3489
kou-pg-0131 <kou.pg.0131@gmail.com>
2020-12-24 16:17:10 +0900

    レポートのフォーマットを修正

--- src/interfaces/presenters/reports_presenter.go
+++ src/interfaces/presenters/reports_presenter.go
@@ -48,9 +48,10 @@ func (p *ReportsPresenter) Weekly(from time.Time, items *domain.Items) (string, 
 	rows = append(rows, strings.Join(tags, " "))
 	rows = append(rows, "")
 
-	rows = append(rows, "# 集計期間")
+	rows = append(rows, "# 集計について")
 	rows = append(rows, "")
-	rows = append(rows, fmt.Sprintf("%s ~ %s", from.Format("2006-01-02"), from.AddDate(0, 0, 7).Format("2006-01-02")))
+	rows = append(rows, fmt.Sprintf("- 期間: %s ~ %s", from.Format("2006-01-02"), from.AddDate(0, 0, 7).Format("2006-01-02")))
+	rows = append(rows, "- 条件: ストック数が **15** 以上の記事")
 	rows = append(rows, "")
 
 	rows = append(rows, "# LGTM 数ランキング")
@@ -102,9 +103,10 @@ func (p *ReportsPresenter) WeeklyPerTag(from time.Time, items *domain.Items, tag
 	rows = append(rows, strings.Join(tags, " "))
 	rows = append(rows, "")
 
-	rows = append(rows, "# 集計期間")
+	rows = append(rows, "# 集計について")
 	rows = append(rows, "")
-	rows = append(rows, fmt.Sprintf("%s ~ %s", from.Format("2006-01-02"), from.AddDate(0, 0, 7).Format("2006-01-02")))
+	rows = append(rows, fmt.Sprintf("- 期間: %s ~ %s", from.Format("2006-01-02"), from.AddDate(0, 0, 7).Format("2006-01-02")))
+	rows = append(rows, "- 条件: ストック数が **2** 以上の記事")
 	rows = append(rows, "")
 
 	rows = append(rows, "# LGTM 数ランキング")
