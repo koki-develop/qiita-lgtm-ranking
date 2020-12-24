e00626930f6442b2ca838c1508b91ce65a638ba7
kou-pg-0131 <kou.pg.0131@gmail.com>
2020-12-24 21:50:12 +0900

    ItemsRepository は GetAll() のみにした

--- src/interfaces/gateways/items_repository.go
+++ src/interfaces/gateways/items_repository.go
@@ -1,16 +1,12 @@
 package gateways
 
 import (
-	"fmt"
-	"time"
-
 	"github.com/kou-pg-0131/qiita-lgtm-ranking/src/domain"
 )
 
 // IItemsRepository .
 type IItemsRepository interface {
-	GetAll(from time.Time) (*domain.Items, error)
-	GetAllWithTag(from time.Time, tag string) (*domain.Items, error)
+	GetAll(query string) (*domain.Items, error)
 }
 
 // ItemsRepository .
@@ -24,26 +20,7 @@ func NewItemsRepository(api IQiitaAPI) *ItemsRepository {
 }
 
 // GetAll .
-func (r *ItemsRepository) GetAll(from time.Time) (*domain.Items, error) {
-	query := fmt.Sprintf("created:>=%s stocks:>=10", from.Format("2006-01-02"))
-	items, err := r.getAll(query)
-	if err != nil {
-		return nil, err
-	}
-	return items, nil
-}
-
-// GetAllWithTag .
-func (r *ItemsRepository) GetAllWithTag(from time.Time, tag string) (*domain.Items, error) {
-	query := fmt.Sprintf("created:>=%s stocks:>=2 tag:%s", from.Format("2006-01-02"), tag)
-	items, err := r.getAll(query)
-	if err != nil {
-		return nil, err
-	}
-	return items, nil
-}
-
-func (r *ItemsRepository) getAll(query string) (*domain.Items, error) {
+func (r *ItemsRepository) GetAll(query string) (*domain.Items, error) {
 	items := &domain.Items{}
 
 	for i := 1; i <= 100; i++ {
--- src/interfaces/gateways/items_repository_test.go
+++ src/interfaces/gateways/items_repository_test.go
@@ -3,7 +3,6 @@ package gateways
 import (
 	"errors"
 	"testing"
-	"time"
 
 	"github.com/kou-pg-0131/qiita-lgtm-ranking/src/domain"
 	"github.com/stretchr/testify/assert"
@@ -28,13 +27,13 @@ func Test_NewItemsRepository(t *testing.T) {
 
 func TestItemsRepository_GetAll_ReturnItemsWhenSucceeded(t *testing.T) {
 	mapi := new(mockQiitaAPI)
-	mapi.On("GetItems", 1, 100, "created:>=2020-01-01 stocks:>=10").Return(&domain.Items{{Title: "TITLE_1"}}, nil)
-	mapi.On("GetItems", 2, 100, "created:>=2020-01-01 stocks:>=10").Return(&domain.Items{{Title: "TITLE_2"}}, nil)
-	mapi.On("GetItems", 3, 100, "created:>=2020-01-01 stocks:>=10").Return(&domain.Items{}, nil)
+	mapi.On("GetItems", 1, 100, "QUERY").Return(&domain.Items{{Title: "TITLE_1"}}, nil)
+	mapi.On("GetItems", 2, 100, "QUERY").Return(&domain.Items{{Title: "TITLE_2"}}, nil)
+	mapi.On("GetItems", 3, 100, "QUERY").Return(&domain.Items{}, nil)
 
 	r := &ItemsRepository{qiitaAPI: mapi}
 
-	items, err := r.GetAll(time.Date(2020, 1, 1, 0, 0, 0, 0, time.UTC))
+	items, err := r.GetAll("QUERY")
 
 	assert.Equal(t, &domain.Items{
 		{Title: "TITLE_1"},
@@ -46,46 +45,11 @@ func TestItemsRepository_GetAll_ReturnItemsWhenSucceeded(t *testing.T) {
 
 func TestItemsRepository_GetAll_ReturnErrorWhenGetItemsFailed(t *testing.T) {
 	mapi := new(mockQiitaAPI)
-	mapi.On("GetItems", 1, 100, "created:>=2020-01-01 stocks:>=10").Return((*domain.Items)(nil), errors.New("SOMETHING_WRONG"))
+	mapi.On("GetItems", 1, 100, "QUERY").Return((*domain.Items)(nil), errors.New("SOMETHING_WRONG"))
 
 	r := &ItemsRepository{qiitaAPI: mapi}
 
-	items, err := r.GetAll(time.Date(2020, 1, 1, 0, 0, 0, 0, time.UTC))
-
-	assert.Nil(t, items)
-	assert.EqualError(t, err, "SOMETHING_WRONG")
-	mapi.AssertNumberOfCalls(t, "GetItems", 1)
-}
-
-/*
- * ItemsRepository.GetAllWithTag()
- */
-
-func TestItemsRepository_GetAllWithTag_ReturnItemsWhenSucceeded(t *testing.T) {
-	mapi := new(mockQiitaAPI)
-	mapi.On("GetItems", 1, 100, "created:>=2020-01-01 stocks:>=2 tag:TAG").Return(&domain.Items{{Title: "TITLE_1"}}, nil)
-	mapi.On("GetItems", 2, 100, "created:>=2020-01-01 stocks:>=2 tag:TAG").Return(&domain.Items{{Title: "TITLE_2"}}, nil)
-	mapi.On("GetItems", 3, 100, "created:>=2020-01-01 stocks:>=2 tag:TAG").Return(&domain.Items{}, nil)
-
-	r := &ItemsRepository{qiitaAPI: mapi}
-
-	items, err := r.GetAllWithTag(time.Date(2020, 1, 1, 0, 0, 0, 0, time.UTC), "TAG")
-
-	assert.Equal(t, &domain.Items{
-		{Title: "TITLE_1"},
-		{Title: "TITLE_2"},
-	}, items)
-	assert.Nil(t, err)
-	mapi.AssertNumberOfCalls(t, "GetItems", 3)
-}
-
-func TestItemsRepository_GetAllWithTag_ReturnErrorWhenGetItemsFailed(t *testing.T) {
-	mapi := new(mockQiitaAPI)
-	mapi.On("GetItems", 1, 100, "created:>=2020-01-01 stocks:>=2 tag:TAG").Return((*domain.Items)(nil), errors.New("SOMETHING_WRONG"))
-
-	r := &ItemsRepository{qiitaAPI: mapi}
-
-	items, err := r.GetAllWithTag(time.Date(2020, 1, 1, 0, 0, 0, 0, time.UTC), "TAG")
+	items, err := r.GetAll("QUERY")
 
 	assert.Nil(t, items)
 	assert.EqualError(t, err, "SOMETHING_WRONG")
