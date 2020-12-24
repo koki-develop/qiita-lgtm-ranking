d0fb4a6e27cff00fe4e308e4f0fbf2ffa1370aea
kou-pg-0131 <kou.pg.0131@gmail.com>
2020-12-24 12:08:14 +0900

    Items のテストを実装

---
+++ src/domain/item_test.go
@@ -0,0 +1,51 @@
+package domain
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+/*
+ * ItemsHasLGTM()
+ */
+
+func TestItems_HasLGTM(t *testing.T) {
+	items := &Items{
+		{Title: "1", LikesCount: 1},
+		{Title: "2", LikesCount: 0},
+		{Title: "3", LikesCount: 10},
+		{Title: "4", LikesCount: 0},
+		{Title: "5", LikesCount: 100},
+	}
+
+	assert.Equal(t, &Items{
+		{Title: "1", LikesCount: 1},
+		{Title: "3", LikesCount: 10},
+		{Title: "5", LikesCount: 100},
+	}, items.HasLGTM())
+}
+
+/*
+ * Items.SortByLikesCount()
+ */
+
+func TestItems_SortByLikesCount(t *testing.T) {
+	items := &Items{
+		{Title: "1", LikesCount: 1},
+		{Title: "2", LikesCount: 0},
+		{Title: "3", LikesCount: 10},
+		{Title: "4", LikesCount: 0},
+		{Title: "5", LikesCount: 100},
+	}
+
+	items.SortByLikesCount()
+
+	assert.Equal(t, &Items{
+		{Title: "5", LikesCount: 100},
+		{Title: "3", LikesCount: 10},
+		{Title: "1", LikesCount: 1},
+		{Title: "2", LikesCount: 0},
+		{Title: "4", LikesCount: 0},
+	}, items)
+}
