0a62d17361d5895652536ee03ef0105d77c09269
kou-pg-0131 <kou.pg.0131@gmail.com>
2020-12-24 12:22:00 +0900

    ReportsController のテストを実装

---
+++ src/interfaces/controllers/mocks_test.go
@@ -0,0 +1,35 @@
+package controllers
+
+import (
+	"time"
+
+	"github.com/kou-pg-0131/qiita-lgtm-ranking/src/domain"
+	"github.com/stretchr/testify/mock"
+)
+
+type mockItemsRepository struct {
+	mock.Mock
+}
+
+func (m *mockItemsRepository) GetAll(from time.Time, tag string) (*domain.Items, error) {
+	args := m.Called(from, tag)
+	return args.Get(0).(*domain.Items), args.Error(1)
+}
+
+type mockReportsRepository struct {
+	mock.Mock
+}
+
+func (m *mockReportsRepository) Update(id, body, tag string) error {
+	args := m.Called(id, body, tag)
+	return args.Error(0)
+}
+
+type mockReportsPresenter struct {
+	mock.Mock
+}
+
+func (m *mockReportsPresenter) WeeklyPerTag(from time.Time, items *domain.Items, tag string) (string, error) {
+	args := m.Called(from, items, tag)
+	return args.String(0), args.Error(1)
+}
---
+++ src/interfaces/controllers/reports_controller_test.go
@@ -0,0 +1,95 @@
+package controllers
+
+import (
+	"errors"
+	"testing"
+	"time"
+
+	"github.com/kou-pg-0131/qiita-lgtm-ranking/src/domain"
+	"github.com/stretchr/testify/assert"
+)
+
+/*
+ * ReportsController.UpdateWeeklyPerTag()
+ */
+
+func TestReportsController_UpdateWeeklyPerTag_ReturnNilWhenSucceeded(t *testing.T) {
+	mir := new(mockItemsRepository)
+	mir.On("GetAll", time.Date(2020, 1, 1, 0, 0, 0, 0, time.UTC), "TAG").Return(&domain.Items{{Title: "TITLE"}}, nil)
+
+	mrp := new(mockReportsPresenter)
+	mrp.On("WeeklyPerTag", time.Date(2020, 1, 1, 0, 0, 0, 0, time.UTC), &domain.Items{{Title: "TITLE"}}, "TAG").Return("BODY", nil)
+
+	mrr := new(mockReportsRepository)
+	mrr.On("Update", "REPORT_ID", "BODY", "TAG").Return(nil)
+
+	c := &ReportsController{
+		itemsRepository:   mir,
+		reportsPresenter:  mrp,
+		reportsRepository: mrr,
+	}
+
+	err := c.UpdateWeeklyPerTag(time.Date(2020, 1, 8, 0, 0, 0, 0, time.UTC), "REPORT_ID", "TAG")
+
+	assert.Nil(t, err)
+	mir.AssertNumberOfCalls(t, "GetAll", 1)
+	mrp.AssertNumberOfCalls(t, "WeeklyPerTag", 1)
+	mrr.AssertNumberOfCalls(t, "Update", 1)
+}
+
+func TestReportsController_UpdateWeeklyPerTag_ReturnErrorWhenGetAllFailed(t *testing.T) {
+	mir := new(mockItemsRepository)
+	mir.On("GetAll", time.Date(2020, 1, 1, 0, 0, 0, 0, time.UTC), "TAG").Return((*domain.Items)(nil), errors.New("SOMETHING_WRONG"))
+
+	c := &ReportsController{
+		itemsRepository: mir,
+	}
+
+	err := c.UpdateWeeklyPerTag(time.Date(2020, 1, 8, 0, 0, 0, 0, time.UTC), "REPORT_ID", "TAG")
+
+	assert.EqualError(t, err, "SOMETHING_WRONG")
+	mir.AssertNumberOfCalls(t, "GetAll", 1)
+}
+
+func TestReportsController_UpdateWeeklyPerTag_ReturnErrorWhenPresenterFailed(t *testing.T) {
+	mir := new(mockItemsRepository)
+	mir.On("GetAll", time.Date(2020, 1, 1, 0, 0, 0, 0, time.UTC), "TAG").Return(&domain.Items{{Title: "TITLE"}}, nil)
+
+	mrp := new(mockReportsPresenter)
+	mrp.On("WeeklyPerTag", time.Date(2020, 1, 1, 0, 0, 0, 0, time.UTC), &domain.Items{{Title: "TITLE"}}, "TAG").Return("", errors.New("SOMETHING_WRONG"))
+
+	c := &ReportsController{
+		itemsRepository:  mir,
+		reportsPresenter: mrp,
+	}
+
+	err := c.UpdateWeeklyPerTag(time.Date(2020, 1, 8, 0, 0, 0, 0, time.UTC), "REPORT_ID", "TAG")
+
+	assert.EqualError(t, err, "SOMETHING_WRONG")
+	mir.AssertNumberOfCalls(t, "GetAll", 1)
+	mrp.AssertNumberOfCalls(t, "WeeklyPerTag", 1)
+}
+
+func TestReportsController_UpdateWeeklyPerTag_ReturnUpdateFailed(t *testing.T) {
+	mir := new(mockItemsRepository)
+	mir.On("GetAll", time.Date(2020, 1, 1, 0, 0, 0, 0, time.UTC), "TAG").Return(&domain.Items{{Title: "TITLE"}}, nil)
+
+	mrp := new(mockReportsPresenter)
+	mrp.On("WeeklyPerTag", time.Date(2020, 1, 1, 0, 0, 0, 0, time.UTC), &domain.Items{{Title: "TITLE"}}, "TAG").Return("BODY", nil)
+
+	mrr := new(mockReportsRepository)
+	mrr.On("Update", "REPORT_ID", "BODY", "TAG").Return(errors.New("SOMETHING_WRONG"))
+
+	c := &ReportsController{
+		itemsRepository:   mir,
+		reportsPresenter:  mrp,
+		reportsRepository: mrr,
+	}
+
+	err := c.UpdateWeeklyPerTag(time.Date(2020, 1, 8, 0, 0, 0, 0, time.UTC), "REPORT_ID", "TAG")
+
+	assert.EqualError(t, err, "SOMETHING_WRONG")
+	mir.AssertNumberOfCalls(t, "GetAll", 1)
+	mrp.AssertNumberOfCalls(t, "WeeklyPerTag", 1)
+	mrr.AssertNumberOfCalls(t, "Update", 1)
+}
