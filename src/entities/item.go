68849763c4041c7ac5e89607aa60702bcc6a9986
kou-pg-0131 <kou.pg.0131@gmail.com>
2021-08-14 16:37:28 +0900

    構造体ごとにファイルをわけた

--- src/entities/item.go
+++ src/entities/item.go
@@ -5,7 +5,6 @@ import (
 	"time"
 )
 
-// Item .
 type Item struct {
 	Title      string    `json:"title"`
 	LikesCount int       `json:"likes_count"`
@@ -15,24 +14,8 @@ type Item struct {
 	CreatedAt  time.Time `json:"created_at"`
 }
 
-// Items .
 type Items []*Item
 
-// Tag .
-type Tag struct {
-	Name string `json:"name"`
-}
-
-// Tags .
-type Tags []*Tag
-
-// User .
-type User struct {
-	ID              string `json:"id"`
-	ProfileImageURL string `json:"profile_image_url"`
-}
-
-// HasLGTM .
 func (items *Items) HasLGTM() *Items {
 	rtn := &Items{}
 
@@ -45,7 +28,6 @@ func (items *Items) HasLGTM() *Items {
 	return rtn
 }
 
-// SortByLikesCount .
 func (items *Items) SortByLikesCount() {
 	sort.SliceStable(*items, func(i, j int) bool {
 		return (*items)[i].LikesCount > (*items)[j].LikesCount
