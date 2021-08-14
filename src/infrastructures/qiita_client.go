84275cbf1c1d9c8c296a948e45226814ff5ccb2c
kou-pg-0131 <kou.pg.0131@gmail.com>
2021-08-14 14:19:18 +0900

    QiitaClient をリファクタ

--- src/infrastructures/qiita_client.go
+++ src/infrastructures/qiita_client.go
@@ -2,93 +2,91 @@ package infrastructures
 
 import (
 	"bytes"
-	"errors"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"strconv"
-	"strings"
 
 	"github.com/kou-pg-0131/qiita-lgtm-ranking/src/entities"
+	"github.com/pkg/errors"
 )
 
-// QiitaAPI .
-type QiitaAPI struct {
-	accessToken   string
-	httpClient    IHTTPClient
-	jsonDecoder   IJSONDecoder
-	jsonMarshaler IJSONMarshaler
+type QiitaClient struct {
+	accessToken string
+	httpAPI     HTTPAPI
 }
 
-// NewQiitaAPI .
-func NewQiitaAPI(accessToken string) *QiitaAPI {
-	return &QiitaAPI{
-		accessToken:   accessToken,
-		httpClient:    NewHTTPClient(),
-		jsonDecoder:   NewJSONDecoder(),
-		jsonMarshaler: NewJSONMarshaler(),
+func NewQiitaClient(accessToken string) *QiitaClient {
+	return &QiitaClient{
+		accessToken: accessToken,
+		httpAPI:     http.DefaultClient,
 	}
 }
 
-// GetItems .
-func (api *QiitaAPI) GetItems(page, perPage int, query string) (*entities.Items, error) {
+func (c *QiitaClient) GetItems(page, perPage int, query string) (entities.Items, error) {
 	req, err := http.NewRequest("GET", "https://qiita.com/api/v2/items", nil)
 	if err != nil {
-		return nil, err
+		return nil, errors.WithStack(err)
 	}
-
 	q := req.URL.Query()
 	q.Add("page", strconv.Itoa(page))
 	q.Add("per_page", strconv.Itoa(perPage))
 	q.Add("query", query)
 	req.URL.RawQuery = q.Encode()
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", c.accessToken))
 
-	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", api.accessToken))
-
-	resp, err := api.httpClient.Do(req)
+	resp, err := c.httpAPI.Do(req)
 	if err != nil {
 		return nil, err
 	}
-	if resp.StatusCode != 200 {
-		buf := new(bytes.Buffer)
-		buf.ReadFrom(resp.Body)
-		return nil, errors.New(buf.String())
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		b, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return nil, errors.WithStack(err)
+		}
+		return nil, errors.New(string(b))
 	}
 
-	items := &entities.Items{}
-	if err := api.jsonDecoder.Decode(resp.Body, items); err != nil {
-		return nil, err
+	var items entities.Items
+	if err := json.NewDecoder(resp.Body).Decode(&items); err != nil {
+		return nil, errors.WithStack(err)
 	}
 
 	return items, nil
 }
 
-// UpdateItem .
-func (api *QiitaAPI) UpdateItem(id, title, body string, tags entities.Tags) error {
-	b, err := api.jsonMarshaler.Marshal(map[string]interface{}{
+func (c *QiitaClient) UpdateItem(id, title, body string, tags entities.Tags) error {
+	p, err := json.Marshal(map[string]interface{}{
 		"title": title,
 		"body":  body,
 		"tags":  tags,
 	})
 	if err != nil {
-		return err
+		return errors.WithStack(err)
 	}
 
-	req, err := http.NewRequest("PATCH", fmt.Sprintf("https://qiita.com/api/v2/items/%s", id), strings.NewReader(string(b)))
+	req, err := http.NewRequest("PATCH", fmt.Sprintf("https://qiita.com/api/v2/items/%s", id), bytes.NewReader(p))
 	if err != nil {
-		return err
+		return errors.WithStack(err)
 	}
-
 	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", api.accessToken))
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", c.accessToken))
 
-	resp, err := api.httpClient.Do(req)
+	resp, err := c.httpAPI.Do(req)
 	if err != nil {
-		return err
+		return errors.WithStack(err)
 	}
-	if resp.StatusCode != 200 {
-		buf := new(bytes.Buffer)
-		buf.ReadFrom(resp.Body)
-		return errors.New(buf.String())
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		b, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return errors.WithStack(err)
+		}
+		return errors.New(string(b))
 	}
 
 	return nil
