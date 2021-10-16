a4138fcc274466901440737bb2497904babd7af5
koki-develop <kou.pg.0131@gmail.com>
2021-10-16 13:19:37 +0900

    SNS のサブスクリプションを作成

--- terraform/modules/aws/sns.tf
+++ terraform/modules/aws/sns.tf
@@ -1,3 +1,9 @@
 resource "aws_sns_topic" "errors" {
   name = "${local.prefix}-errors"
 }
+
+resource "aws_sns_topic_subscription" "errors_email" {
+  topic_arn = aws_sns_topic.errors.arn
+  protocol  = "email"
+  endpoint  = "kou.pg.0131@gmail.com"
+}
