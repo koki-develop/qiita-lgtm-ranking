62bc3ff3fda95056fa1575a8bcf90511be2d2bb2
koki-develop <kou.pg.0131@gmail.com>
2022-08-21 10:56:31 +0900

    ReportController.UpdateDaily を実装

---
+++ pkg/controllers/reports.go
@@ -0,0 +1,62 @@
+package controllers
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/koki-develop/qiita-lgtm-ranking/pkg/infrastructures/qiita"
+	"github.com/koki-develop/qiita-lgtm-ranking/pkg/infrastructures/report"
+)
+
+type ReportController struct {
+	builder     *report.Builder
+	qiitaClient *qiita.Client
+}
+
+type ReportControllerConfig struct {
+	QiitaAccessToken string
+}
+
+func NewReportController(cfg *ReportControllerConfig) *ReportController {
+	return &ReportController{
+		builder:     report.NewBuilder(),
+		qiitaClient: qiita.New(cfg.QiitaAccessToken),
+	}
+}
+
+func (ctrl *ReportController) UpdateDaily(rptID string) error {
+	now := time.Now()
+	from := now.AddDate(0, 0, -1)
+	query := fmt.Sprintf("created:>=%s", from.Format("2006-01-02"))
+
+	var items qiita.Items
+	for i := 0; i < 100; i++ {
+		rslt, err := ctrl.qiitaClient.GetItems(&qiita.GetItemsOptions{Page: i + 1, PerPage: 100, Query: query})
+		if err != nil {
+			return err
+		}
+		items = append(items, rslt...)
+		if len(rslt) < 100 {
+			break
+		}
+	}
+
+	rpt, err := ctrl.builder.Build(from, now, items)
+	if err != nil {
+		return err
+	}
+
+	if err := ctrl.qiitaClient.UpdateItem(rptID, &qiita.UpdateItemPayload{
+		Title: "Qiita デイリー LGTM 数ランキング【自動更新】",
+		Body:  rpt,
+		Tags: qiita.Tags{
+			{Name: "Qiita"},
+			{Name: "lgtm"},
+			{Name: "ランキング"},
+		},
+	}); err != nil {
+		return err
+	}
+
+	return nil
+}
