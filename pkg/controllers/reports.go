a106c631955f7e4793bbb314b52b90996784b168
koki-develop <kou.pg.0131@gmail.com>
2022-08-21 15:01:56 +0900

    weeklybytag を実装

--- pkg/controllers/reports.go
+++ pkg/controllers/reports.go
@@ -239,3 +239,75 @@ func (ctrl *ReportController) UpdateWeekly(rptID string) error {
 
 	return nil
 }
+
+func (ctrl *ReportController) UpdateWeeklyByTag(rptID, tag string) error {
+	now := time.Now()
+	from := now.AddDate(0, 0, -7)
+	query := fmt.Sprintf("created:>=%s stocks:>=2 tag:%s", from.Format("2006-01-02"), tag)
+
+	var items qiita.Items
+	for i := 0; i < 100; i++ {
+		rslt, err := ctrl.qiitaClient.GetItems(&qiita.GetItemsOptions{Page: i + 1, PerPage: 100, Query: query})
+		if err != nil {
+			return err
+		}
+		items = append(items, rslt.FilterWithMinLiked(1)...)
+		if len(rslt) < 100 {
+			break
+		}
+	}
+
+	for _, item := range items {
+		cnt, err := ctrl.qiitaClient.GetStockersCount(item.ID)
+		if err != nil {
+			return err
+		}
+		item.StockersCount = cnt
+	}
+
+	files, err := filepath.Glob("./events/updateWeeklyByTag/*.prod.json")
+	if err != nil {
+		return errors.WithStack(err)
+	}
+
+	var tags report.Tags
+	for _, file := range files {
+		f, err := os.Open(file)
+		if err != nil {
+			return errors.WithStack(err)
+		}
+		defer f.Close()
+		var tag report.Tag
+		if err := json.NewDecoder(f).Decode(&tag); err != nil {
+			return errors.WithStack(err)
+		}
+		tags = append(tags, &tag)
+	}
+
+	rpt, err := ctrl.builder.Build(&report.BuildOptions{
+		Tags: tags,
+		Conditions: report.Conditions{
+			{Key: "期間", Value: fmt.Sprintf("%s ~ %s", from.Format("2006-01-02"), now.Format("2006-01-02"))},
+			{Key: "条件", Value: "ストック数が **2** 以上の記事"},
+		},
+		Items: items,
+	})
+	if err != nil {
+		return err
+	}
+
+	if err := ctrl.qiitaClient.UpdateItem(rptID, &qiita.UpdateItemPayload{
+		Title: fmt.Sprintf("【%s】Qiita 週間 LGTM 数ランキング【自動更新】", tag),
+		Body:  rpt,
+		Tags: qiita.Tags{
+			{Name: "Qiita"},
+			{Name: "lgtm"},
+			{Name: "ランキング"},
+			{Name: tag},
+		},
+	}); err != nil {
+		return err
+	}
+
+	return nil
+}
